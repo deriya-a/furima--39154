3e8fd8a09fca33f7a92c20448ece0e77651be4af
deriya-a <derya0806@gmail.com>
2023-05-22 20:22:20 +0900

    テスト用アカウントを設定
    FactoryBot使用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :user do
-    ##furimaに合わせたユーザー情報を設定します
-    name {Faker::Name.last_name}
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    nickname {Faker::Name.last_name}
+    family_name_kanji { "山田" }
+    first_name_kanji { "太郎" }
+    family_name_kana { "ヤマダ" }
+    first_name_kana { "タロウ" }
+    birthday { Date.new(1983, 1, 1) }
   end
 end

cb78f1cdd4a806f47c171f62bb08038d15bea045
deriya-a <derya0806@gmail.com>
2023-05-22 21:54:52 +0900

    Update Userモデル単体テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+
+    it 'emailが空では登録できない' do
+      @user.email = ''  # emailの値を空にする
+      @user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+      # nicknameが空では登録できないテストコードを記述します
+    end
+    it 'passwordが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+    it 'nicknameが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+    it 'family_name_kanjiが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+    it 'first_name_kanjiが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+    it 'family_name_kanaが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+    it 'first_name_kanaが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+    it 'birthdayが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+
+  end
 end

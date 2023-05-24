7bcc18b96ad8dce7597858d1307d21f6284e4d81
deriya-a <derya0806@gmail.com>
2023-05-24 20:05:59 +0900

    Update Userモデル単体テストコード
    エラーメッセージの記述を更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "半角英数字混合で入力してください" }
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: '半角英数字混合で入力してください' }
 
   validates :nickname, presence: true
-  validates :family_name_kanji, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}ー－]+\z/, message: "漢字で入力してください" }
-  validates :first_name_kanji, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}ー－]+\z/, message: "漢字で入力してください" }
-  validates :family_name_kana, presence: true, format: { with: /\A[\p{Katakana}ー－]+\z/, message: "カタカナで入力してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}ー－]+\z/, message: "カタカナで入力してください" }
+  validates :family_name_kanji, presence: true,
+                                format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}ー－]+\z/, message: '漢字で入力してください' }
+  validates :first_name_kanji, presence: true,
+                               format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}ー－]+\z/, message: '漢字で入力してください' }
+  validates :family_name_kana, presence: true, format: { with: /\A[\p{Katakana}ー－]+\z/, message: 'カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}ー－]+\z/, message: 'カタカナで入力してください' }
   validates :birthday, presence: true
 
   has_many :items
   has_many :records
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    nickname {Faker::Name.last_name}
-    family_name_kanji { "山田" }
-    first_name_kanji { "太郎" }
-    family_name_kana { "ヤマダ" }
-    first_name_kana { "タロウ" }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false) }
+    password_confirmation { password }
+    nickname { Faker::Name.last_name }
+    family_name_kanji { '山田' }
+    first_name_kanji { '太郎' }
+    family_name_kana { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
     birthday { Date.new(1983, 1, 1) }
   end
 end

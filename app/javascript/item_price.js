28a4dee70d84548f053acac2c658584b4fe6d8b0
deriya-a <derya0806@gmail.com>
2023-06-24 16:23:55 +0900

    Update item_price.js
    販売価格、販売手数料、販売利益の計算をJSファイルに記述

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,25 +1,40 @@
-//window.addEventListener('load', () => {
-  //const inputField = document.getElementById('item-price');
-  //console.log(inputField);
-//});
+window.addEventListener('load', () => {
+  const inputField = document.getElementById('item-price')
+  inputField.addEventListener("input", () => {
+  
+  const inputValue = inputField.value
+  
+  const addTaxDom = document.getElementById("add-tax-price")
+  const addTaxPrice = inputValue * 0.1
+  const roundedTaxPrice = Math.floor(addTaxPrice)
+  addTaxDom.innerHTML = roundedTaxPrice
+  
+
+  const profitAmount = document.getElementById("profit")
+  const salesProfit = inputValue - addTaxPrice
+  const roundedProfit = Math.floor(salesProfit)
+  profitAmount.innerHTML = roundedProfit
+  
+})
+})
 
 // HTML要素の取得
-const inputField = document.getElementById('item-price');
+//const inputField = document.getElementById('item-price');
 
-const resultElement = document.getElementById('add-tax-price');
+//const resultElement = document.getElementById('add-tax-price');
 
 // イベントリスナーの設定
-inputField.addEventListener('input', calculateTax);
+//inputField.addEventListener('input', calculateTax);
 
 // 10%の税金を計算する関数
-function calculateTax() {
+//function calculateTax() {
   // 入力された値を取得
-  const inputValue = parseFloat(inputField.value);
+  //const inputValue = parseFloat(inputField.value);
   
   // 10%の税金を計算
-  const taxAmount = inputValue * 0.1;
+  //const taxAmount = inputValue * 0.1;
   
   // 結果を表示
-  resultElement.textContent = taxAmount.toFixed(2);
-}
+  //resultElement.textContent = taxAmount.toFixed(2);
+//}
 

822345ae4f2a6d280948de98cfc6509645f71f2c
deriya-a <derya0806@gmail.com>
2023-06-24 18:40:53 +0900

    Update item.rb　Itemモデルファイル
    ・アクティブハッシュのdelivery_feeとdelivery_withinファイルのモデル名を命名規則に沿って修正
    ・Itemモデルにアクティブハッシュの1.---が入力できないようにバリデーションを追加

--- app/models/delivery_within.rb
+++ app/models/delivery_within.rb
@@ -1,4 +1,4 @@
-class Delivery_within < ActiveHash::Base
+class DeliveryWithin < ActiveHash::Base
   self.data = [
     { id: 1, name: "---" },
     { id: 2, name: "1~2日で発送" },
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
 
   validates :name,               presence: { message: '入力してください' }
   validates :detail,             presence: { message: '入力してください' }
-  validates :category_id,        presence: { message: '選択してください' }
-  validates :condition_id,       presence: { message: '選択してください' }
-  validates :delivery_fee_id,    presence: { message: '選択してください' }
-  validates :region_id,          presence: { message: '選択してください' }
-  validates :delivery_within_id, presence: { message: '選択してください' }
+  validates :category_id,        exclusion: { in: [1], message: '選択してください' }, presence: true
+  validates :condition_id,       exclusion: { in: [1], message: '選択してください' }, presence: true
+  validates :delivery_fee_id,    exclusion: { in: [1], message: '選択してください' }, presence: true
+  validates :region_id,          exclusion: { in: [1], message: '選択してください' }, presence: true
+  validates :delivery_within_id, exclusion: { in: [1], message: '選択してください' }, presence: true
   validates :price, presence: { message: '入力してください' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
  

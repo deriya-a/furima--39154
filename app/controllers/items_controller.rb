c1a04e44b14775475bf23aac93fd23bb22ac1e00
deriya-a <derya0806@gmail.com>
2023-07-02 21:26:25 +0900

    Update itemコントローラー
    一覧の表示を降順に更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # before_action :ensure_author, only: [:edit, :update]
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new

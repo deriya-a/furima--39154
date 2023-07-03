790b37c7cad52b4a8e99e19d02ea64ae7c784c64
deriya-a <derya0806@gmail.com>
2023-07-03 18:51:24 +0900

    編集結果保存の記述を追加
    ・ルーティング追記
    ・編集ページへのパス追記
    ・edit.html.erb更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  # before_action :authenticate_user!, only: [:edit, :update, :destroy]
-  # before_action :ensure_author, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  # before_action :authenticate_user!, only: [:destroy]
+  before_action :ensure_author, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -24,10 +24,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end 
+
+  def update
+       @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :detail, :category_id, :condition_id, :delivery_fee_id, :region_id,
                                  :delivery_within_id, :price).merge(user_id: current_user.id)
   end
+
+  def ensure_author
+    @item = Item.find(params[:id])
+    unless @item.user == current_user
+      flash[:alert] = "編集・更新は出品者のみが行えます"
+      redirect_to root_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show] do
+  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
   
   end
-  #resources :items, only: [:edit, :update, :destroy] do
+  #resources :items, only: [:destroy] do
     
   
 end

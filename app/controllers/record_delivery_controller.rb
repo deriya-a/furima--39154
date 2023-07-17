f778df83d6f83fdc03e6b256683ace8279c30686
deriya-a <derya0806@gmail.com>
2023-07-17 17:38:39 +0900

    Update record_delivery_controller.rb
    private記述

--- app/controllers/record_delivery_controller.rb
+++ app/controllers/record_delivery_controller.rb
@@ -7,6 +7,20 @@ class RecordDeliveryController < ApplicationController
   end
 
   def create
+    @record = Record.create(record_params)
+    Delilvery.create(delivery_params)
+    redirect_to root_path
   end
-  
+
+
+  private
+
+  def record_params
+    params.merge(user_id: current_user.id, item_id: curernt_item.id)
+  end
+
+  def delivery_params
+    params.permit(:post_code, :region_id, :address1, :address2, :address3, :phone).merge(record_id: @record.id)
+  end
+
 end

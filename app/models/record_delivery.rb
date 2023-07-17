9890e30ed9db41148b603581bfc83841e8878adf
deriya-a <derya0806@gmail.com>
2023-07-17 14:42:58 +0900

    Update record_delivery.rb
    バリデーション記述

--- app/models/record_delivery.rb
+++ app/models/record_delivery.rb
@@ -1,19 +1,25 @@
 class RecordDelivery
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :region_id, :address1, :address2, :address3, :phone, :reecord_id
+  attr_accessor :user_id, :item_id, :post_code, :region_id, :address1, :address2, :address3, :phone, :record_id
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is invalid"}
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :region_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :address1
+    validates :address2
+    validates :phone
+    validates :reecord_id
+    
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+    validates :address3
 
   def save
-    # 寄付情報を保存し、変数donationに代入する
-    donation = Donation.create(price: price, user_id: user_id)
+    # 購入情報を保存し、変数recordに代入する
+    record = Record.create(user_id: user_id, item_id: item_id)
     # 住所を保存する
-    # donation_idには、変数donationのidと指定する
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, donation_id: donation.id)
+    # record_idには、変数recordのidと指定する
+    Delivery.create(post_code: post_code, region_id: region_id, address1: address1, address2: address2, address3: address3, phone: phone, record_id: record.id)
   end
 end
